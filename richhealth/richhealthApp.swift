3f2c1a8095f00af459d4845e47c0dbbaf34c01ba
amitplays <streetw@yahoo.com>
2026-08-06 18:08:07 -0400

    Initial Commit

---
+++ richhealth/richhealthApp.swift
@@ -0,0 +1,17 @@
+//
+//  richhealthApp.swift
+//  richhealth
+//
+//  Created by Ams on 2026-08-06.
+//
+
+import SwiftUI
+
+@main
+struct richhealthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
